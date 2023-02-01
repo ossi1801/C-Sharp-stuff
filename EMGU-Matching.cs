0afc7502525ab5611cb93465c20ff71831eaaec8
Ossi H <ossihakki@gmail.com>
2023-02-01 22:54:46 +0200

    added "best" match of multiple images

--- EMGU-Matching.cs
+++ EMGU-Matching.cs
@@ -30,4 +30,33 @@ internal class Program
         CvInvoke.Resize(img, img, new Size(0, 0), 0.5, 0.5);
         CvInvoke.Imshow("Find on bgr", img);
     }
+    public static void findBestPlaceOnMap(Image<Bgr, byte> img, List<Image<Bgr, byte>> templates)
+    {
+        List<double> minVals = new List<double>();
+        List<double> maxVals = new List<double>();
+        List<Point> minLocs = new List<Point>();
+        List<Point> maxLocs = new List<Point>();    
+        for (int i = 0; i < templates.Count; i++)
+        {
+            var result = new Mat();
+            CvInvoke.MatchTemplate(img, templates[i], result, TemplateMatchingType.SqdiffNormed); //When using SqdiffNormed use lowest values [minVal,minLoc,Min()]
+            double minVal = Double.MaxValue;
+            double maxVal = Double.MinValue;
+            var minLoc = new Point();
+            var maxLoc = new Point();
+            var mask = new Mat();
+            CudaInvoke.MinMaxLoc(result, ref minVal, ref maxVal, ref minLoc, ref maxLoc, mask);
+            minVals.Add(minVal);
+            maxVals.Add(maxVal);
+            minLocs.Add(minLoc);
+            maxLocs.Add(maxLoc);
+            //Console.WriteLine(i+"/360 "+":" +maxVal);
+        }
+        int index  = minVals.IndexOf(minVals.Min());
+        var maxPoint = minLocs[index];
+        var color1 = new MCvScalar(0, 255, 0);
+        var point1 = new Point(maxPoint.X + templates[index].Cols, maxPoint.Y + templates[index].Rows);
+        CvInvoke.Rectangle(img, new Rectangle(point1, ((Size)minLocs[index])), color1);
+        CvInvoke.Imshow("Find tmp", img);
+    }
 }
